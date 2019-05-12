bd054b8fb7736f8020031a0a1b2f130e67c0a228
pavel <leskovecpavel7@gmail.com>
2019-05-12 13:54:34 +0300

    Updates

---
+++ precache-manifest.3f922aaf4876f9f83606051480217317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac86e891f5c35e29778d63aae3b4a044",
+    "url": "/movie-app/static/media/Morpheus.ac86e891.jpg"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  },
+  {
+    "revision": "64cf4fd96fb15bb6f6b6",
+    "url": "/movie-app/static/js/main.bc7699d1.chunk.js"
+  },
+  {
+    "revision": "e60138e52a9dc5491588",
+    "url": "/movie-app/static/js/2.f6ef0bc0.chunk.js"
+  },
+  {
+    "revision": "64cf4fd96fb15bb6f6b6",
+    "url": "/movie-app/static/css/main.44d015fb.chunk.css"
+  },
+  {
+    "revision": "0c383b60d5d5214533d95a5877176d4b",
+    "url": "/movie-app/index.html"
+  }
+];
